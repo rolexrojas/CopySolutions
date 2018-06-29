8787f0849040b09c8426f2d74ff21b41b2b828e9
CODETEL\Roberto Rojas <roberto_rojas@claro.com.do>
2018-06-29 16:02:38 -0400

    BASIC LAYOUT SETUP INCOMPLETE DINAMIC CONTENT LOADER

---
+++ application/controllers/Contact.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of contactController
+ *
+ * @author Roberto Rojas
+ */
+class Contact extends CI_Controller{
+    //put your code here
+    
+    public function index(){
+        
+        $this->load->view('Contacts_view');
+        
+    }
+}
---
+++ application/views/Homepage_view.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+
+  <head>
+   <?php include("layout/header.php"); ?>
+  </head>
+
+  <body>
+
+   <?php include("layout/navbar.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h1 class="mt-5">Bienvenidos a Copy Solutions</h1>
+          <p class="lead">Una bolsada para el Sr. Bienvenido Dalmau!</p>
+          <ul class="list-unstyled">
+            <li>Bootstrap 4.1.1</li>
+            <li>jQuery 3.3.1</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+   <?php include("layout/footer.php"); ?>
+
+  </body>
+
+</html>
